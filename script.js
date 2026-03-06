4f83b3b0d5f716713c2979db9655456483ae559d
Tushar kumar <narutoboi41@gmail.com>
2026-03-06 15:22:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //complete this code
 class Person {
 	constructor(name,age){
-        this.name = name
-        this.age = age
+        this._name = name
+        this._age = age
     }
     name(){
-       return this.name
+       return this._name
     }
     age(){
-        return this.age
+        return this._age
     }
 }
 
